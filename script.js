41fcb5c49bae827e4d1d4dd143fcfeb5db9bcf42
Víctor Aarón Bárcenas Mondragón <aaronbarcenasm@gmail.com>
2024-12-15 20:24:54 -0600

    Actualizacion de pagina con seccion de portafolio y descripcion de paginas

--- script.js
+++ script.js
@@ -13,3 +13,30 @@ var typed = new Typed(".text" , {
   backDelay: 1000,
   loop:true
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+  resaltarEnlace()
+})
+
+function resaltarEnlace() {
+  document.addEventListener('scroll', function() {
+    const sections = document.querySelectorAll('section')
+    const navLinks = document.querySelectorAll('.navegacion li a')
+
+    let actual = '';
+    sections.forEach( section => {
+      const sectionTop = section.offsetTop
+      const sectionHeight = section.clientHeight
+      if( window.scrollY >= (sectionTop - sectionHeight / 3)){
+        actual = section.id
+      }
+    })
+
+    navLinks.forEach(link => {
+      link.classList.remove('select')
+      if(link.getAttribute('href') === '#' + actual) {
+        link.classList.add('select')
+      }
+    })
+  })
+}
